c184468055a1d510f73d275d23d0af5b0280fbb9
luan048 <luanmonteiropedrosa@gmail.com>
2024-11-01 23:49:04 -0300

    Front of the Home Page Created

--- DesafioReact/src/App.jsx
+++ DesafioReact/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import './App.css'
+
 function App() {
-    return <h1>TESTE TESTE</h1>
+    return (
+        <div className="divHomePage">
+            <p className="pSearch">Search Devs</p>
+            <div className="elementsHomePage">
+                <input className="inputHomePage" type="text" placeholder="Type the username here..."/>
+                <button className="buttonHomePage"><i className="fa-solid fa-magnifying-glass"/> Buscar</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
